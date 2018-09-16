8a9ce861aa4c24bf75ea8bcd8d21f596a043c45d
stanford <stanfordlin@outlook.com>
2018-09-16 13:59:33 -0400

    added template

---
+++ ncr-template.js
@@ -0,0 +1,68 @@
+{
+    "items": [
+        {
+            "version": 1,
+            "packageIdentifiers": [
+                {
+                    "type": "foodname",
+                    "value": "Kiwi, Golden"
+                },
+                {
+                    "type": "brand_name",
+                    "value": "Zespri"
+                },
+                {
+                    "type": "calories",
+                    "value": "215"
+                },
+                {
+                    "type": "sodium",
+                    "value": "215"
+                },
+                {
+                    "type": "carbohydrate",
+                    "value": "10"
+                },
+                {
+                    "type": "calcium",
+                    "value": "215"
+                },
+                {
+                    "type": "iron",
+                    "value": "0.8"
+                },
+                {
+                    "type": "fibre",
+                    "value": "0.4"
+                },
+                {
+                    "type": "VitmainA",
+                    "value": "1"
+                }, 
+                {
+                    "type": "VitaminB",
+                    "value": "1"
+                }, 
+                {
+                    "type": "VitaminC",
+                    "value": "1"
+                }
+            ],
+            "shortDescription": {
+                "values": [
+                    {
+                        "locale": "en-US",
+                        "value": "String"
+                    }
+                ]
+            },
+            "merchandiseCategory": {
+                "nodeId": "null"
+            },
+            "itemId": {
+                "itemCode": "13"
+            },
+            "departmentId": "produce"
+        }
+    ]
+}
